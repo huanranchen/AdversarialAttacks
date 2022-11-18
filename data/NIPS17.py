5d399475b32fb059b8fef6ee5c1641001bfa9571
Andrew Chen <1062446749@qq.com>
2022-11-18 14:37:37 +0800

    chr: update

--- data/NIPS17.py
+++ data/NIPS17.py
@@ -1,4 +1,8 @@
+from torch.utils.data import Dataset, DataLoader
+
+
 __kaggle_link__ = 'kaggle datasets download -d google-brain/nips-2017-adversarial-learning-development-set'
 
 
-
+class NIPS17(Dataset):
+    def __init__(self, images_path = './resources/NIPS17/images'):
