d2a437663eb6f94f2e92681bb66c18c35df91a91
huanranchen <huanran_chen@outlook.com>
2023-01-24 20:55:23 +0800

    chr: ddim

--- defenses/DiffPure/diffusions.py
+++ defenses/DiffPure/diffusions.py
@@ -154,32 +154,85 @@ def ddpm(unet, device, betas, alpha, alpha_bar, ):
     return x
 
 
-def ddim(unet, device, alpha_bar, stride=50):
-    x = torch.randn((1, 3, 256, 256), device=device)
-    alpha_bar = alpha_bar[::stride]
-    embedding_t = list(range(1000))[::stride]
-    for t in range(alpha_bar.shape[0] - 1, 0, -1):
-        # sigma = torch.sqrt(1 - alpha[t]) * torch.sqrt(1 - alpha_bar[t - 1]) / torch.sqrt(1 - alpha_bar[t])
-        sigma = 0
-        tensor_t = torch.zeros((x.shape[0]), device=device) + embedding_t[t]
-        predict = unet(x, tensor_t)[:, :3, :, :]
-        x0 = (x - torch.sqrt(1 - alpha_bar[t]) * predict) / torch.sqrt(alpha_bar[t])
-        if t > 1:
-            noise = torch.randn_like(x) * sigma
-        else:
-            noise = 0
-        x = torch.sqrt(alpha_bar[t - 1]) * x0 + torch.sqrt(1 - alpha_bar[t - 1] - sigma ** 2) * predict + noise
-    return x
+class DDIM(nn.Module):
+    def __init__(self, unet=None, beta=None, T=1000, stride=1):
+        super(DDIM, self).__init__()
+        if unet is None:
+            unet, beta = get_unet()
+        if beta is None:
+            beta = torch.linspace(0.1 / 1000, 20 / 1000, 1000, device=torch.device('cuda'))
+        self.device = torch.device('cuda')
+        self.unet = unet
+        alpha = (1 - beta)
+        self.alpha_bar = alpha.cumprod(dim=0).to(self.device)
+        self.beta = beta
+        self.T = T
+        self.eval().to(self.device).requires_grad_(False)
+        self.noise_type = "diagonal"
+        self.sde_type = "ito"
+        self.to_img = transforms.ToPILImage()
+        self.i = 0
+        self.stride = stride
+
+    def convert(self, x):
+        x = (x + 1) * 0.5
+        img = self.to_img(x[0])
+        img.save(f'./what/{self.i}.png')
+        self.i += 1
+        return x
+
+    def sample(self, stride=1):
+        x = torch.randn((1, 3, 256, 256), device=self.device)
+        alpha_bar = self.alpha_bar[::stride]
+        embedding_t = list(range(1000))[::stride]
+        for t in range(alpha_bar.shape[0] - 1, 0, -1):
+            # sigma = torch.sqrt(1 - alpha[t]) * torch.sqrt(1 - alpha_bar[t - 1]) / torch.sqrt(1 - alpha_bar[t])
+            sigma = 0
+            tensor_t = torch.zeros((x.shape[0]), device=self.device) + embedding_t[t]
+            predict = self.unet(x, tensor_t)[:, :3, :, :]
+            x0 = (x - torch.sqrt(1 - alpha_bar[t]) * predict) / torch.sqrt(alpha_bar[t])
+            if t > 1:
+                noise = torch.randn_like(x) * sigma
+            else:
+                noise = 0
+            x = torch.sqrt(alpha_bar[t - 1]) * x0 + torch.sqrt(1 - alpha_bar[t - 1] - sigma ** 2) * predict + noise
+        return x
+
+    def purify(self, x, noise_level=150):
+        x = (x - 0.5) * 2
+        x = torch.sqrt(self.alpha_bar[noise_level - 1]) * x + \
+            torch.randn_like(x, requires_grad=False) * torch.sqrt(1 - self.alpha_bar[noise_level - 1])
+
+        alpha_bar = self.alpha_bar[:noise_level][::self.stride]
+        embedding_t = list(range(self.T))[:noise_level][::self.stride]
+        for t in range(alpha_bar.shape[0] - 1, 0, -1):
+            # sigma = torch.sqrt(1 - alpha[t]) * torch.sqrt(1 - alpha_bar[t - 1]) / torch.sqrt(1 - alpha_bar[t])
+            sigma = 0
+            tensor_t = torch.zeros((x.shape[0]), device=self.device) + embedding_t[t]
+            predict = self.unet(x, tensor_t)[:, :3, :, :]
+            x0 = (x - torch.sqrt(1 - alpha_bar[t]) * predict) / torch.sqrt(alpha_bar[t])
+            if t > 1:
+                noise = torch.randn_like(x) * sigma
+            else:
+                noise = 0
+            x = torch.sqrt(alpha_bar[t - 1]) * x0 + torch.sqrt(1 - alpha_bar[t - 1] - sigma ** 2) * predict + noise
+        x = self.convert(x)
+        return x
+
+    def __call__(self, *args, **kwargs):
+        return self.purify(*args, **kwargs)
 
 
 class DiffusionPureImageNet(nn.Module):
-    def __init__(self, mode='sde'):
+    def __init__(self, mode='sde', *args, **kwargs):
         super(DiffusionPureImageNet, self).__init__()
         self.device = torch.device('cuda')
         if mode == 'sde':
-            self.diffusion = DiffusionSde()
+            self.diffusion = DiffusionSde(*args, **kwargs)
         elif mode == 'ode':
-            self.diffusion = DiffusionOde()
+            self.diffusion = DiffusionOde(*args, **kwargs)
+        elif mode == 'ddim':
+            self.diffusion = DDIM(*args, **kwargs)
         self.model = resnet50(pretrained=True)
         self.eval().requires_grad_(False)
         self.to(self.device)
